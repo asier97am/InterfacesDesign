29e93b60fd541445159a5f8e02e1f8db3f434339
Asier Aranda <asier.aranda.malagon@gmail.com>
2021-11-11 09:39:21 +0100

    solo falta icono de aplicacion

--- app/src/main/java/com/asier/aranda/diseointerfaces/SignUp.java
+++ app/src/main/java/com/asier/aranda/diseointerfaces/SignUp.java
@@ -1,8 +1,12 @@
 package com.asier.aranda.diseointerfaces;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ImageView mAcantilado=findViewById(R.id.backAcantilado1);
+        Glide.with(this)
+                .load(R.drawable.acantilado)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .into(mAcantilado);
     }
 }
