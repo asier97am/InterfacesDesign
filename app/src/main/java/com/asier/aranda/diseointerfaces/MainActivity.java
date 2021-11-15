232ae4b53d83af0ab4051cf577092e9011c948bc
Asier Aranda <asier.aranda.malagon@gmail.com>
2021-11-15 13:15:00 +0100

    Snackbar implementado en Copy

--- app/src/main/java/com/asier/aranda/diseointerfaces/MainActivity.java
+++ app/src/main/java/com/asier/aranda/diseointerfaces/MainActivity.java
@@ -2,13 +2,17 @@ package com.asier.aranda.diseointerfaces;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
+
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         // casting a la vista a la que aplicamos un menu contextual
         // y la registramos
@@ -34,9 +37,25 @@ public class MainActivity extends AppCompatActivity {
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.item1:
-                Toast toast = Toast.makeText(this, "Item Copied", Toast.LENGTH_LONG);
-                toast.show();
-                return true ;
+//                Toast toast = Toast.makeText(this, "Item copied",
+//                        Toast.LENGTH_LONG);
+//                toast.show();
+
+                final ConstraintLayout mLayout =  findViewById(R.id.myMainConstraint);
+
+                Snackbar snackbar = Snackbar
+                        .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+                        .setAction("Deshacer", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                                snackbar1.show();
+                            }
+                        });
+
+                snackbar.show();
+                return true;
+
 
             case R.id.item2:
                 Toast toast2 = Toast.makeText(this, "Downloading", Toast.LENGTH_LONG);
